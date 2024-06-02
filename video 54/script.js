ddc4e1db5145be4350b578b4e2c6bcb4e01a60a3
Khushwant <khush123098@gmail.com>
2024-06-02 22:51:48 +0530

    js start

---
+++ video 54/script.js
@@ -0,0 +1,17 @@
+alert("Hello World!")
+
+console.log("This should show in the console")
+console.log("This should also show in the console")
+
+var num = prompt("Enter your number: ")
+console.log("Your number is " + num)
+
+var isTrue = confirm("Do you want to continue or not?")
+
+if(isTrue) {
+    console.log("Continuing")
+}
+
+else {
+    console.log("Not continuing")
+}
