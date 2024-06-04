690e59f61dbd020a08ac7343c0cafcfd72f446d8
Khushwant <khush123098@gmail.com>
2024-06-04 21:54:36 +0530

    ez

---
+++ video 56/index.js
@@ -0,0 +1,37 @@
+console.log("Hello, this is tutorial 56")
+
+let age = 45;
+
+console.log("The age is " + age + ".");
+
+if (age > 18) {
+    console.log("You can drive.");
+}
+else {
+    console.log("You can not drive.");
+}
+
+// arithmetic operators were taught, already familiar with them
+let a = 15;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
+
+// assignment operators
+/* 
+    =   x = y;
+    +=  x = x + y;
+    -=  x = x - y;
+    etc
+*/
+
+// comparison operators
+/* 
+    ===     equal value and type
+    !==     not equal value or not equal type
+*/
