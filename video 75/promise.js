30755f9b01bb020f99860385f5e7d11635d644d6
Khushwant <khush123098@gmail.com>
2024-06-23 00:09:34 +0530

    js promises

--- video 75/promise.js
+++ video 75/promise.js
@@ -5,13 +5,35 @@ let prom1 = new Promise((resolve, reject) => {
     if (a < 0.5) {
         reject("No, the random number is not supported.");
     }
-    setTimeout(() => {
-        console.log('Yes, I am done');
-        resolve("Khushwant");
-    }, 3000);
+    else {
+        setTimeout(() => {
+            console.log('Yes, I am done');
+            resolve("Khushwant");
+        }, 3000);
+    }
 })
 
-prom1.then((a) => {
+// prom1.then((a) => {
+//     console.log(a);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No, the random number is not supported 2");
+    }
+    else {
+        setTimeout(() => {
+            console.log('Yes, I am done 2');
+            resolve("Khushwant 2");
+        }, 1000);
+    }
+})
+
+let p3 = Promise.all([prom1, prom2]);
+p3.then((a) => {
     console.log(a);
 }).catch((err) => {
     console.log(err);
