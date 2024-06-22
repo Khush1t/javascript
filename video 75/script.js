71fe0ceb1d2dc78e50191fc56e7345e806dea6d9
Khushwant <khush123098@gmail.com>
2024-06-22 23:29:47 +0530

    callback

--- video 75/script.js
+++ video 75/script.js
@@ -9,3 +9,17 @@ setTimeout(() => {
 }, 100);
 
 console.log("This is the end statement");
+
+
+const callback = (arg) => {
+    console.log(arg);
+}
+
+const loadScript = (src, callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    sc.onload = callback("khushwant");
+    document.head.append;
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback);
