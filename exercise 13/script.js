8cf5d2e7f902e4c1072ad241d311a0c1996eb9a7
Khushwant <khush123098@gmail.com>
2024-06-24 23:08:28 +0530

    exercise 13

---
+++ exercise 13/script.js
@@ -0,0 +1,5 @@
+function createCard(title, channel_name, views, monthsOld, duration, thumbnail) {
+    let html = ``
+}
+
+createCard("Introduction to Web Development #1", "PocoLoco", 7500000, 2, "28:03", "https://as2.ftcdn.net/v2/jpg/02/66/72/41/1000_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpgˀ")
