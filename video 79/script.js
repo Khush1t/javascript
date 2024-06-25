e0576d357417365b106fe64b3e55b424c6b7e364
Khushwant <khush123098@gmail.com>
2024-06-25 22:20:22 +0530

    js try & catch

--- video 79/script.js
+++ video 79/script.js
@@ -5,7 +5,7 @@ let b = prompt("Enter second number: ");
 // this returns a string
 // let sum = a + b;
 
-if(isNaN(a) || isNaN(b)) {
+if (isNaN(a) || isNaN(b)) {
     throw SyntaxError("Error, only numbers are allowed.")
 }
 
@@ -13,9 +13,21 @@ let sum = parseInt(a) + parseInt(b);
 
 // console.log('The sum is ' + sum);
 
-try {
-    console.log('The sum is ' + sum * x);
-} catch (error) {
-    console.log('Error aa gaya bhai');   
-    console.log(error.name);
+// FINALLY will be executed even if we encounter a return statement before it
+// otherwise, the code stops after a return statement
+function main() {
+    let x = 1;
+    try {
+        console.log('The sum is ' + sum * x);
+        return true;
+    } catch (error) {
+        console.log('Error aa gaya bhai');
+        console.log(error.name);
+        return false;
+    }
+    finally {
+        console.log("The code execution is completed and db connection is being closed.");
+    }
 }
+
+let c = main();
